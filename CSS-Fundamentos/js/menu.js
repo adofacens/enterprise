7959df8c8fd71788035831c1174e7239df1dda4c
adofacens <abimael.facens@gmail.com>
2024-11-26 20:11:19 -0300

    update

---
+++ CSS-Fundamentos/js/menu.js
@@ -0,0 +1,37 @@
+// Arquivo das automações do Menu Hamburguer
+
+// Selecionando o botão menu pelo seu ID utilizando 'const' para não 
+// modificar posteriormente
+const btnMobile = document.getElementById('btn-mobile');
+
+/**
+ * Função que alterna o estado do menu (aberto/fechado)
+ * @param {Event} - Evento que dispara a função, como 'click' ou 'touchstart'
+  */
+ function toggleMenu(event) {
+  //Evita o comportamento padrão do evento 'touchstart', evitando bug em dispositivos móveis
+  if (event.type === 'touchstart') event.preventDefault(); 
+
+  // Seleciona o elemnto de navegação pelo seu ID
+  const nav = document.getElementById('nav');
+
+  // Adiciona ou remove a classe 'active' no elemnto de navegação, ativando ou desativando o menu
+  nav.classList.toggle('active');
+
+  // Verifica se o menu está aberto ou não
+  const isActive = nav.classList.contains('active');
+
+  // Atualiza o atributo 'aria-expanded' para melhorar a acessibilidade ao menu
+  event.currentTarget.setAttribute('aria-expanded', isActive);
+
+  // Define o texto do atributo 'aria-label' depedendo do estado do menu (aberto/fechado)
+  if (isActive) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+ }
+
+//  Adiciona eventos de 'click' e 'tocuhstart' para ativar a função toggleMenu ao interagir com o botão
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
